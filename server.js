95ca2cd136773d65917e50109c6bf907570ea9f6
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-11-04 22:23:49 +0200

    feat: handle routes with nodemon and apply get request

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const server = require("express");
 
-const app = express();
+const app = server();
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/hi", (req, res) => {
+  res.send("Hi momo");
+});
+app.get("/test", (req, res) => {
+  res.send("you test now");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
