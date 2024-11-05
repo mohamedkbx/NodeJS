d94dc9f7f18e1bc299a608a629a411cc68b3f1c0
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-11-05 17:22:00 +0200

    feat: create response with body-path-query params

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const server = require("express");
 
 const app = server();
 
+app.use(server.json());
+
 app.get("/hello", (req, res) => {
-  res.send("Hello World!");
+  res.json({ message: res.body.msg });
 });
 
 app.get("/hi", (req, res) => {
@@ -14,8 +16,10 @@ app.get("/test", (req, res) => {
   res.send("you test now");
 });
 
-app.get("/numbers", (req, res) => {
-  res.send("you test now");
+app.get("/findSum/:number1/:number2", (req, res) => {
+  const num1 = req.params.number1;
+  const num2 = req.params.number2;
+  console.log(num1, num2);
 });
 
 app.post("/addCommet", (req, res) => {
